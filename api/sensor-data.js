4e851aae5b2877260bce9711646f5681e001bbd7
hs7ma <131772070+hs7ma@users.noreply.github.com>
2026-01-01 15:10:28 +0300

    Add files via upload

--- api/sensor-data.js
+++ api/sensor-data.js
@@ -7,7 +7,7 @@ let latestSensorData = {
   timestamp: Date.now()
 };
 
-export default function handler(req, res) {
+module.exports = function handler(req, res) {
   // Enable CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
